4bf715447600858a29034b81324b909194e8c147
Kalin Motovilkov <monyllakk@abv.bg>
2019-12-24 13:28:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'game.php' ?>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <meta charset="UTF-8">
+    <title>Rock - Paper - Scissors</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<div id="game">
+    <?php game(); ?>
+</div>
+</body>
+</html>
